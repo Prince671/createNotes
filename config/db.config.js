276bad52b2fa6df349934fe06a006e3987913187
Prince soni <157681120+Prince671@users.noreply.github.com>
2025-06-22 23:07:50 +0530

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
+
 console.log("📦 Starting connection...");
 
 const mongoose = require('mongoose');
 
-mongoose.connect(
-  'mongodb+srv://prince67:PrinceS3210@cluster0.6y4zijx.mongodb.net/testDB?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log("✅ Connected to MongoDB Atlas"))
 .catch((err) => console.error("❌ Connection error:", err));
